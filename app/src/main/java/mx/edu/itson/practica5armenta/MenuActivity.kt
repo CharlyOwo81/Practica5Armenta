0a56a5b59e7aa27216948f61d5a898247a3290b9
oloo <ooo>
2025-02-18 19:25:17 -0700

    Segundo Commit 18 de febrero

--- app/src/main/java/mx/edu/itson/practica5armenta/MenuActivity.kt
+++ app/src/main/java/mx/edu/itson/practica5armenta/MenuActivity.kt
@@ -15,11 +15,42 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnAntojitos:Button = findViewById(R.id.antojitosBtn) as Button
+        var btnEspecialidades:Button = findViewById(R.id.especialidadesBtn) as Button
+        var btnCombinaciones:Button = findViewById(R.id.combinationsBtn) as Button
+        var btnTortas:Button = findViewById(R.id.tortasBtn) as Button
+        var btnSopas:Button = findViewById(R.id.sopasBtn) as Button
+        var btnBebidas:Button = findViewById(R.id.drinksBtn) as Button
 
         btnAntojitos.setOnClickListener {
             var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","Antojitos")
             startActivity(intent)
         }
 
+        btnEspecialidades.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","Especialidades")
+            startActivity(intent)
+        }
+        btnCombinaciones.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","Combos")
+            startActivity(intent)
+        }
+        btnTortas.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","Tortas")
+            startActivity(intent)
+        }
+        btnSopas.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","Sopas")
+            startActivity(intent)
+        }
+        btnBebidas.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","Drinks")
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/mx/edu/itson/practica5armenta/ProductosActivity.kt
+++ app/src/main/java/mx/edu/itson/practica5armenta/ProductosActivity.kt
@@ -31,9 +31,85 @@ class ProductosActivity : AppCompatActivity() {
         listview.adapter = adaptador
     }
 
-    fun agregarProductos(){
+    fun agregarProductos(option:String?){
         when(option){
-
+            "Antojitos"->{
+                menu.add(Product("Quesadillas",R.drawable.quesadillas, "\n" +
+                        "Rellenas con su carne favorita, servidas con ensalada - Filled with your " +
+                        "choice of meat, served with salad.",5.99))
+                menu.add(Product("Huaraches",R.drawable.huaraches, "\n" +
+                        "Tortilla gruesa con frijoles, tu carne favorita, lechuga, queso fresco y " +
+                        "crema - Big Thick tortilla with beans, your choice of meat, fresh cheese, " +
+                        "and sour cream.",10.99))
+                menu.add(Product("Gringas",R.drawable.gringas, "\n" +
+                        "Tortilla de harina con queso, carne al pastor y piña - Flour tortilla filled " +
+                        "with cheese, marinated pork and pineapple.",7.99))
+                menu.add(Product("Sincronizadas",R.drawable.sincronizadas, "\n" +
+                        "Tortilla de haring rellena con queso y jamón. Acompañada de lechuga, crema y " +
+                        "guacamole - Two four tortillas filled with ham and cheese. Served with lettuce, " +
+                        "sour cream, and guacamole.",7.99))
+                menu.add(Product("Sopes",R.drawable.sopes, "\n" +
+                        "Tortilla gruesa cubierta de frijoles, tu carne favorita, lechuga, queso fresco " +
+                        "y crema - Fried thick tortilla with beans, your choice of meat, lettuce, fresh " +
+                        "cheese, and sour cream.",3.79))
+                menu.add(Product("Tostadas",R.drawable.tostadas, "\n" +
+                        "Tortilla frita con frijoles, tu carne favorita, lechuga, queso fresco, crema y " +
+                        "jitomate - Fried tortilla with beans, your choice of meat, lettuce, fresh cheese," +
+                        " sour cream and tomatoes.",4.59))
+            }
+            "Especialidades"->{
+                menu.add(Product("Mojarra Frita",R.drawable.mojarra, "\n" +
+                        "Tilapia frita servida con lechuga, cebolla, jitomate, aguacate y tortillas - " +
+                        "Fried tilapia served with lettuce, onions, tomatoes, avocado and tortillas.",17.99))
+                menu.add(Product("Ceviche",R.drawable.ceviche, "\n" +
+                        "Tilapia, camarones, Aguacate y pico de gallo servido con Chips / Tilapia, " +
+                        "Shrimp, Avocado and pico de gallo, served with Chips",6.99))
+                menu.add(Product("Botana de camarones al Mojo de Ajo",R.drawable.botanacamarones, "\n" +
+                        "Instrucciones especiales",19.99))
+                menu.add(Product("Botana de camarones a la Diabla",R.drawable.botanacamarones, "\n" +
+                        "Instrucciones especiales",19.99))
+                menu.add(Product("Torre de Camarón Negro",R.drawable.botanacamarones, "\n" +
+                        "Instrucciones especiales",16.99))
+                menu.add(Product("Botana de pulpo a la Diabla",R.drawable.botanacamarones, "\n" +
+                        "Servido totopos, cebolla frita, arroz, lechuga y pico de gallo - Served with chips, " +
+                        "grill onions, rice, lettuce and pico de gallo.",24.99))
+            }
+            "Combos"->{
+                menu.add(Product("Traditional Taco Combo",R.drawable.combinationtaco, "\n" +
+                        "\n" +
+                        "Tortilla de Maíz y tu carne favorita. Servido con arroz y frijoles - " +
+                        "Corn taco, your choice of meat, served with rice and beans.",6.99))
+                menu.add(Product("Mexican Burritos",R.drawable.cominationburros, "\n" +
+                        "Tu carne favorita en una tortilla grande de harina rellena con cebolla frita, " +
+                        "pico de gallo, guacamole, sour cream, frijoles y acompañado de arroz - " +
+                        "Your choice of meat on a big burrito made of flour tortilla with rice, beans, " +
+                        "lettuce, grilled onions, pico de gallo, guacamole and sour cream.",12.99))
+                menu.add(Product("Los Portales Burrito",R.drawable.burritosportales, "\n" +
+                        "Large flour tortilla filled with grilled cactus, steak, pastor (seasoned pork), " +
+                        "grilled onions, rice and beans. Served with a side of lettuce salad, tomatoes, " +
+                        "sour cream and guacamole.",13.59))
+                menu.add(Product("Cinco",R.drawable.cinco, "\n" +
+                        "One hard sell taco with your choice of meat, lettuce and " +
+                        "cheese. One burrito with your choice of meat, pico de gallo, " +
+                        "sour cream, guacamole and beans. Served with rice and beans.",9.99))
+                menu.add(Product("Dos Amigos",R.drawable.dosamigos, "\n" +
+                        "Two hard shell tacos with your choice of meat topped with lettuce " +
+                        "and shredded cheese. served with a side of rice and beans",9.99))
+                menu.add(Product("Burritos Pepe",R.drawable.burritopepe, "\n" +
+                        "\n" +
+                        "Flour tortilla filled with your choice of meat with beans, lettuce, " +
+                        "sour cream, guacamole and pico de gallo. served with a side of rice.",9.99))
+                menu.add(Product("Fajita Quesadilla",R.drawable.fajitaquesadilla, "\n" +
+                        "Gran burrito relleno de tu carne favorita, cebolla, chile, arroz, frijoles y " +
+                        "jitomate. Servido con lechuga y guacamole - Large burrito filled with your " +
+                        "choice of meat, onions, chile, peppers, rice, beans and tomatoes. " +
+                        "Served with lettuce guacamole and sour cream.",13.99))
+            }
+            "Tortas"->{
+                menu.add(Product("Torta Regular",R.drawable.torta, "\n" +
+                        "All tortas come with lettuce, onions, tomatoes, avocado, sour cream, " +
+                        "jalapeno pepper and your choice of meat.",12.99))
+            }
         }
     }
 }
